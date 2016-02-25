19fbcd0acd25598d6756541de14b6e85962db19a
Kelson da Costa Medeiros <kelsoncm@gmail.com>
2016-02-25 11:41:33 -0300

    [ADD] initial project structure

---
+++ setup.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from distutils.core import setup
+setup(
+    name='django_model_documentation',
+    packages=['django_brfied',
+              'django_brfied.models'],
+    version='1.0.0',
+    description='Django Application specific brazilian fields types',
+    author='Kelson da Costa Medeiros',
+    author_email='kelsoncm@gmail.com',
+    url='https://github.com/kelsoncm/django_brfied', 
+    download_url='https://github.com/kelsoncm/django_brfied/releases/tag/1.0.0',
+    keywords=['django', 'BR', 'Brazil', 'Brasil', 'model', 'form', 'locale', ],
+    classifiers=[]
+)
