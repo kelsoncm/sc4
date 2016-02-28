6036856ce8e907b23c53757b5dbfb4b53fa1d752
Kelson da Costa Medeiros <kelsoncm@gmail.com>
2016-02-28 16:01:09 -0300

    [ADD] Adding non-python files

--- setup.py
+++ setup.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 from distutils.core import setup
+this_version = '0.1.5'
 setup(
     name='django_brfied',
     packages=['django_brfied',
@@ -7,12 +8,12 @@ setup(
     package_data={
         'django_brfied.static.js': ['*'],
     },
-    version='0.1.4',
+    version=this_version,
+    download_url='https://github.com/kelsoncm/django_brfied/releases/tag/%s' % this_version,
     description='Django Application specific brazilian fields types',
     author='Kelson da Costa Medeiros',
     author_email='kelsoncm@gmail.com',
     url='https://github.com/kelsoncm/django_brfied', 
-    download_url='https://github.com/kelsoncm/django_brfied/releases/tag/0.1.4',
     keywords=['django', 'BR', 'Brazil', 'Brasil', 'model', 'form', 'locale', ],
     classifiers=[]
 )
