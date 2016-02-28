ed0f456d8d5d530f66484a9e51120d9dfe5c9c85
Kelson da Costa Medeiros <kelsoncm@gmail.com>
2016-02-28 19:53:02 -0300

    Release 0.1.6

---
+++ release.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+from distutils.core import setup
+this_version = '0.1.6'
+setup(
+    name='django_brfied',
+    packages=['django_brfied', 'python_brfied', ],
+    package_dir={'django_brfied': 'django_brfied'},
+    package_data={'django_brfied': ['static/js/*'],},
+    version=this_version,
+    download_url='https://github.com/kelsoncm/django_brfied/releases/tag/%s' % this_version,
+    description='Django Application specific brazilian fields types',
+    author='Kelson da Costa Medeiros',
+    author_email='kelsoncm@gmail.com',
+    url='https://github.com/kelsoncm/django_brfied', 
+    keywords=['django', 'BR', 'Brazil', 'Brasil', 'model', 'form', 'locale', ],
+    classifiers=[]
+)
