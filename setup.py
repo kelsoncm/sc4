0ca875d666f1f2fc5665f8d66ffe19e756322af9
Kelson da Costa Medeiros <kelsoncm@gmail.com>
2016-06-19 16:53:51 -0300

    Release 0.1.11

--- setup.py
+++ setup.py
@@ -1,16 +1,23 @@
 # -*- coding: utf-8 -*-
 from distutils.core import setup
 setup(
-    name='django_brfied',
-    packages=['django_brfied', 'python_brfied', ],
-    package_dir={'django_brfied': 'django_brfied'},
-    package_data={'django_brfied': ['static/js/*'],},
-    version='0.1.10',
-    download_url='https://github.com/kelsoncm/django_brfied/releases/tag/0.1.10',
-    description='Django Application specific brazilian fields types',
+    name='django_model_documentation',
+    packages=['django_model_documentation',
+              'django_model_documentation.management',
+              'django_model_documentation.management.commands',
+              'django_model_documentation.templatetags',
+              'django_model_documentation.templates',
+              'django_model_documentation.templates.django_model_documentation', ],
+    package_dir={'django_model_documentation': 'django_model_documentation'},
+    package_data={'django_model_documentation': ['templates/django_model_documentation/*',
+                                                 'static/js/*',
+                                                 'static/css/*', ],},
+    version='0.1.11',
+    download_url='https://github.com/kelsoncm/django_model_documentation/releases/tag/0.1.11',
+    description='Django Application for output a documentation of apps models',
     author='Kelson da Costa Medeiros',
     author_email='kelsoncm@gmail.com',
-    url='https://github.com/kelsoncm/django_brfied',
-    keywords=['django', 'BR', 'Brazil', 'Brasil', 'model', 'form', 'locale', ],
+    url='https://github.com/kelsoncm/django_model_documentation',
+    keywords=['django', 'model', 'documentation', ],    
     classifiers=[]
 )
