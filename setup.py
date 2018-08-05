85c08aa89b96457305ab294da431d19b5f101bfa
Kelson da Costa Medeiros <kelson.medeiros@lais.huol.ufrn.br>
2018-08-05 10:39:51 -0300

    Release 0.8.0

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from distutils.core import setup
 setup(
     name='python_brfied',
     packages=['python_brfied', 'python_brfied.shortcuts', ],
-    version='0.7.1',
-    download_url='https://github.com/kelsoncm/python_brfied/releases/tag/0.7.1',
+    version='0.8.0',
+    download_url='https://github.com/kelsoncm/python_brfied/releases/tag/0.8.0',
     description='Python library specific brazilian validations',
     author='Kelson da Costa Medeiros',
     author_email='kelsoncm@gmail.com',
