3e1ea2703d04feeb00f21e2ea361901c1627a952
Kelson da Costa Medeiros <kelson.medeiros@lais.huol.ufrn.br>
2019-01-01 09:39:34 -0300

    Release 0.1.4

--- setup.py
+++ setup.py
@@ -3,13 +3,14 @@ from distutils.core import setup
 setup(
     name='python_brfied',
     packages=['python_brfied', 'python_brfied.shortcuts', ],
-    version='0.8.5',
-    download_url='https://github.com/kelsoncm/python_brfied/releases/tag/0.8.5',
+    version='%s',
+    download_url='https://github.com/kelsoncm/python_brfied/releases/tag/%s',
     description='Python library specific brazilian validations',
     author='Kelson da Costa Medeiros',
     author_email='kelsoncm@gmail.com',
     url='https://github.com/kelsoncm/python_brfied',
     keywords=['python', 'BR', 'Brazil', 'Brasil', 'model', 'form', 'locale', ],
-    install_requires=['pyfwf==0.1.3', 'requests-ftp==0.3.1', 'requests==2.19.1'],
+    install_requires=['pyfwf==0.1.3', 'requests-ftp==0.3.1', 'requests==2.21.0'],
     classifiers=[]
 )
+
