c4c660df249ddb28306473ef389b8e03f1ec8f41
Kelson da Costa Medeiros <kelsoncm@gmail.com>
2019-11-07 17:29:42 -0300

    Release 0.1.0

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 # -*- coding: utf-8 -*-
 from distutils.core import setup
 setup(
-    name='python_brfied',
-    packages=['python_brfied', 'python_brfied.shortcuts', ],
-    version='0.8.6',
-    download_url='https://github.com/kelsoncm/python_brfied/releases/tag/0.8.6',
+    name='pyshortcuts',
+    packages=['pyshortcuts', 'pyshortcuts.shortcuts', ],
+    version='0.1.0',
+    download_url='https://github.com/kelsoncm/pyshortcuts/releases/tag/0.1.0',
     description='Python library specific brazilian validations',
     author='Kelson da Costa Medeiros',
     author_email='kelsoncm@gmail.com',
-    url='https://github.com/kelsoncm/python_brfied',
+    url='https://github.com/kelsoncm/pyshortcuts',
     keywords=['python', 'BR', 'Brazil', 'Brasil', 'model', 'form', 'locale', ],
     install_requires=['pyfwf==0.1.3', 'requests-ftp==0.3.1', 'requests==2.21.0'],
     classifiers=[]
