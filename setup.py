973fa952355cb1896b4dbbc867c3b581237e001d
Kelson da Costa Medeiros <kelsoncm@gmail.com>
2019-11-07 17:42:17 -0300

    Release 0.1.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from distutils.core import setup
 setup(
     name='pyshortcuts',
-    packages=['pyshortcuts', 'pyshortcuts.shortcuts', ],
+    packages=['pyshortcuts', ],
     version='0.1.0',
     download_url='https://github.com/kelsoncm/pyshortcuts/releases/tag/0.1.0',
     description='Python library specific brazilian validations',
