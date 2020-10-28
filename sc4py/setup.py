18ec3aaedaceacd3ce7531973e80c0f3fa3a6b91
Kelson da Costa Medeiros <kelsoncm@gmail.com>
2020-10-28 10:31:24 -0300

    Release sc4py-v0.1.3

--- sc4py/setup.py
+++ sc4py/setup.py
@@ -3,8 +3,8 @@ from distutils.core import setup
 setup(
     name='sc4py',
     packages=['sc4py', ],
-    version='0.1.2',
-    download_url='https://github.com/kelsoncm/sc4/releases/tag/sc4py-v0.1.2',
+    version='0.1.3',
+    download_url='https://github.com/kelsoncm/sc4/releases/tag/sc4py-v0.1.3',
     description='Python library specific brazilian validations',
     author='Kelson da Costa Medeiros',
     author_email='kelsoncm@gmail.com',
